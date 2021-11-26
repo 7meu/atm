cacbb43dfdeed8a423493ddc3e5d7b26d6739aeb
Gabriel Stanculescu <gabriel.stanculescu@gitscom.ro>
2021-11-26 22:45:12 +0100

    Fix display format, add comments, improve readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-import { MoneyType } from './interfaces/money';
+import { moneyTypes, MoneyType } from './interfaces/money';
 
 function App() {
-  const [view, setView] = useState(0);
-  const [amount, setAmount] = useState('');
-  useEffect(() => {
-    if (view === 0)
-      setView(1)
-  })
+  const [page, setPage] = useState(0); // for changing the displayed page
+  const [amount, setAmount] = useState(''); // for changing the amount to deposit
+  useEffect(() => { // first called when the page fully loads
+    if (page === 0) // when it does
+      setPage(1) // move onto the main page
+  });
 
-  switch (view) {
-    case 1:
+  switch (page) {
+    case 0: // loading page - unlikely to be visible if the app loads quickly
+      return (
+        <body>
+          <h1>Enalyzer</h1>
+          <h1>ATM</h1>
+        </body>
+      )
+
+    case 1: // main page
       return (
         <body>
           <div className="line">
             <div className="center">
-              <p>Select amount</p>
+              <h2>Select amount</h2>
             </div>
           </div>
 
-          
+
           <div className="line">
             <div className="center">
-                <p>£ {amount}|</p>
+              <p className="user-input">£ {amount}|</p>
             </div>
           </div>
 
@@ -54,84 +61,111 @@ function App() {
 
           <div className="line">
             <div className="center">
-              <p className="circle" onClick={() => setAmount(amount.slice(0, -1))}>*</p>
-              <p className="circle" onClick={() => setAmount(amount + '0')}>0</p>
-              <p className="circle">#</p>
+              <p className="circle" onClick={() => setAmount(amount.slice(0, -1))}>←</p>
+              <p className="circle" onClick={() => {
+                if (amount === '')
+                  alert('Amount cannot begin with 0.');
+                else if (amount.length === 9)
+                  alert('You cannot deposit more than £1.000.000.000');
+                else
+                  setAmount(amount + '0')
+              }}>0</p>
+              <p className="circle invisible">#</p>
             </div>
           </div>
 
           <div className="line">
             <div className="center">
-              <div className="submit" onClick={() => setView(2)}>submit</div>
+              <div className="submit" onClick={() => setPage(2)}>submit</div>
             </div>
           </div>
         </body>
       )
 
-    case 2:
-      const moneyTypes = [[1000, MoneyType.Note],
-                          [500, MoneyType.Note],
-                          [200, MoneyType.Note],
-                          [100, MoneyType.Note],
-                          [50, MoneyType.Note],
-                          [20, MoneyType.BigCoin],
-                          [10, MoneyType.SmallCoin],
-                          [5, MoneyType.BigCoin],
-                          [2, MoneyType.BigCoin],
-                          [1, MoneyType.SmallCoin]];
+    case 2: // final page
+      // distributing the user's amount in three lists of tuples
+      // distribution[MoneyType.x] contains the number and value
+      // of notes / coins of type x required to reach the amount
+      const distribution = [[], [], []] as [number, number][][];
 
-      // distributing the user's amount in a list
-      // of tuples of form [note value, number of notes, note type]
-      const distribution = [] as [number, number, MoneyType][];
-      
       let finalAmount = Number(amount);
-      moneyTypes.forEach(it => {
-        if (finalAmount >= it[0])
-          distribution.push([it[0], Math.floor(finalAmount / it[0]), it[1]]);
-          finalAmount %= it[0];
+      moneyTypes.forEach(it => { // for every note / coin type, starting with the highest value ones
+        if (finalAmount >= it[0]) { // if we can make use of the current note / coin
+          distribution[it[1]].push([Math.floor(finalAmount / it[0]), it[0]]); // use as many as possible
+          finalAmount %= it[0]; // continue with the remaining amount
+        }
       });
 
       return (
-        <div>
+        <body>
           <div className="line">
-            <div className="circle" onClick={() => setView(1)}>V</div>
+            <div className="circle" onClick={() => setPage(1)}>←</div>
           </div>
 
           <div className="line">
             <div className="center">
-              <p>Depositing</p>
+              <h2>Depositing</h2>
             </div>
           </div>
 
           <div className="line">
             <div className="center">
-                <p>£ {amount}</p>
+              <h2>£ {amount}</h2>
             </div>
           </div>
           
-          {distribution.map(it => <p>{it[0]}x{it[1]}+</p>)}
-        </div>
+          <div className="table">
+            <div className="column">
+              {distribution[MoneyType.Note].map(it =>
+                <div className="line item">
+                  <div className="white-rectangle">
+                    <div className="invisible-dot"></div>
+
+                  </div>
+                  <div><p>{it[0]} x {it[1]}</p></div>
+
+                </div>
+              )}
+            </div>
+
+            <div className="column">
+              {distribution[MoneyType.BigCoin].map(it =>
+                <div className="line item">
+                  <div className="invisible-rectangle">
+                    <div className="white-dot"></div>
+                  </div>
+
+                  <div><p>{it[0]} x {it[1]}</p></div>
+                </div>
+              )}
+            </div>
+
+            <div className="column">
+              {distribution[MoneyType.SmallCoin].map(it =>
+                <div className="line item">
+                  <div className="invisible-rectangle">
+                    <div className="white-dot"></div>
+                  </div>
+
+                  <p>{it[0]} x {it[1]}</p>
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className="line">
+            <div className="center">
+              <p>Thank you for using Enalyzer ATM</p>
+            </div>
+          </div>
+        </body>
+      )
+
+    default:
+      return (
+        <p>Error!</p>
       )
   }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
 export default App;
--- src/interfaces/money.ts
+++ src/interfaces/money.ts
@@ -3,3 +3,14 @@ export enum MoneyType {
     BigCoin = 1,
     SmallCoin = 2
 }
+
+export const moneyTypes = [[1000, MoneyType.Note],
+                           [500, MoneyType.Note],
+                           [200, MoneyType.Note],
+                           [100, MoneyType.Note],
+                           [50, MoneyType.Note],
+                           [20, MoneyType.BigCoin],
+                           [10, MoneyType.SmallCoin],
+                           [5, MoneyType.BigCoin],
+                           [2, MoneyType.BigCoin],
+                           [1, MoneyType.SmallCoin]];
