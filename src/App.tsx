c293a669be1aaf91fd32d9c30d99433361a25a8b
Gabriel Stanculescu <gabriel.stanculescu@gitscom.ro>
2021-11-26 23:00:14 +0100

    Fix code styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
 import './App.css';
+import { useEffect, useState } from 'react';
 import { moneyTypes, MoneyType } from './interfaces/money';
 
 function App() {
